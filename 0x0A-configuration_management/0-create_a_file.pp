80a7168ce150ee52be31409ef099737a012112d8
Reny kipkoech <57016982+Tr-reny@users.noreply.github.com>
2023-01-10 13:17:51 -0500

    create file

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# Create a file
+file { '/tmp/holberton':
+  ensure  => file,
+  path    => '/tmp/holberton',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet'
+}
