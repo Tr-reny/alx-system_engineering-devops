d077f1e4b06639fbd0d438a29447df58d8485feb
Reny kipkoech <57016982+Tr-reny@users.noreply.github.com>
2023-01-10 14:23:28 -0500

    Update 0-create_a_file.pp

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,9 @@
 # Create a file
-file { '/tmp/school'':
-  ensure  => file,
-  path    => '/tmp/school',
-  mode    => '0744',
-  owner   => 'www-data',
-  group   => 'www-data',
-  content => 'I love Puppet'
+file { '/tmp/school':
+  ensure => file,
+  mode => '0744',
+  owner => 'www-data',
+  group => 'www-data',
+  content => 'I love Puppet',
 }
+
