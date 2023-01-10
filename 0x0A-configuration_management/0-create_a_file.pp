ce1569fc433ff30b39bf4b04cf9693104bfdc812
Reny kipkoech <57016982+Tr-reny@users.noreply.github.com>
2023-01-10 14:11:25 -0500

    Update 0-create_a_file.pp

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,7 @@
 # Create a file
-file { '/tmp/holberton':
+file { '/tmp/school'':
   ensure  => file,
-  path    => '/tmp/holberton',
+  path    => '/tmp/school',
   mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
