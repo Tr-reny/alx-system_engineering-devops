1c0e1c5833584f13ceff4940ed78ad5e0e704971
Reny kipkoech <57016982+Tr-reny@users.noreply.github.com>
2023-02-28 15:51:44 -0500

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,12 +1,19 @@
+#!/usr/bin/python3
+"""
+Queries the Reddit API and returns the number of subscribers for a given subreddit.
+"""
+
 import requests
 
+
 def number_of_subscribers(subreddit):
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    """Returns the number of subscribers for a given subreddit."""
     headers = {'User-Agent': 'Mozilla/5.0'}
-    response = requests.get(url, headers=headers)
-    
+    url = f'https://www.reddit.com/r/{subreddit}/about.json'
+    response = requests.get(url, headers=headers, allow_redirects=False)
+
     if response.status_code != 200:
         return 0
-    
+
     data = response.json()
     return data['data']['subscribers']
