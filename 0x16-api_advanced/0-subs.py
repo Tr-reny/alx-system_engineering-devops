cb903a06b65ca67a2ab6983849c3ee9ef8a23a3f
Reny kipkoech <57016982+Tr-reny@users.noreply.github.com>
2023-02-28 15:33:59 -0500

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,1 +1,12 @@
+import requests
 
+def number_of_subscribers(subreddit):
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    response = requests.get(url, headers=headers)
+    
+    if response.status_code != 200:
+        return 0
+    
+    data = response.json()
+    return data['data']['subscribers']
