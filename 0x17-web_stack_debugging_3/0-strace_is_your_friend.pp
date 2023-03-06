7237c51ab9a981cdeb245d7e052e64b552f2cd05
Reny kipkoech <57016982+Tr-reny@users.noreply.github.com>
2023-03-06 16:07:04 -0500

    Update 0-strace_is_your_friend.pp

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,23 +1,6 @@
-# This Puppet manifest fixes the Apache web server on the server
-
-# Ensure that the apache2 package is installed
-package { 'apache2':
-  ensure => 'installed',
-}
-
-# Fix Apache by restarting the service
-exec { 'fix-apache':
-  command     => 'strace -e trace=file service apache2 restart',
-  path        => '/usr/bin:/usr/sbin:/bin',
-  refreshonly => true,
-}
-
-# Verify that Apache is returning the correct page
-file { '/tmp/test.html':
-  content => '<html><body>Success!</body></html>',
-}
-
-service { 'apache2':
-  ensure => 'running',
-  require => [File['/tmp/test.html'], Exec['fix-apache']],
+# Fix permission issues causing 500 error
+file { '/var/www/html':
+  ensure => directory,
+  owner  => 'www-data',
+  group  => 'www-data',
 }
